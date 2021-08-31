6cd7d47375f9a883c351597aedf82b6a8e09c9f3
Daniel Maizel <dani98000k@gmail.com>
2021-08-31 13:03:42 +0300

    Added dynamic routing
    * Use Trie tree to realize dynamic route analysis.
    * Added wild card routing support

---
+++ gin/trie.go
@@ -0,0 +1,76 @@
+package gin
+
+import (
+	"fmt"
+	"strings"
+)
+
+type node struct {
+	pattern  string  // Route to be matched, for example: /p/:lang
+	part     string  // Part of the route, for example :lang
+	children []*node // Children nodes, for example: [doc, tutorial, into]
+	isWild   bool    // Exact match, true when part contains ":" or "*"
+}
+
+func (n *node) String() string {
+	return fmt.Sprintf("node{pattern=%s, part=%s, isWild=%t}", n.pattern, n.part, n.isWild)
+}
+
+// The first node that successfully matches is used to insert
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+
+	return nil
+}
+
+// All successfully matched nodes are used to find
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+
+	return nodes
+}
+
+func (n *node) insert(pattern string, parts []string, height int) {
+	if len(parts) == height {
+		n.pattern = pattern
+		return
+	}
+
+	part := parts[height]
+	child := n.matchChild(part)
+	if child == nil {
+		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
+		n.children = append(n.children, child)
+	}
+	child.insert(pattern, parts, height+1)
+}
+
+func (n *node) search(parts []string, height int) *node {
+	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
+
+	part := parts[height]
+	children := n.matchChildren(part)
+
+	for _, child := range children {
+		result := child.search(parts, height+1)
+		if result != nil {
+			return result
+		}
+	}
+
+	return nil
+}
