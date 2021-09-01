782d8bb3452b79da74dcd0807fd6f3a1817915c6
Daniel Maizel <dani98000k@gmail.com>
2021-09-01 10:04:19 +0300

    Added route grouping

--- main.go
+++ main.go
@@ -8,20 +8,34 @@ import (
 
 func main() {
 	r := gin.New()
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gin</h1>")
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
-
-	r.GET("/hello", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-
-	r.POST("/login", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello gin</h1>")
 		})
-	})
+
+		v1.GET("/hello", func(c *gin.Context) {
+			// expect /hello?name=gin
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gin.Context) {
+			// expect /hello/gin
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
 
 	r.Run(":9999")
 }
