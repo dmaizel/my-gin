6bab8c0fa978c705a276a883aac02bf5a5a83d94
Daniel Maizel <dani98000k@gmail.com>
2021-09-03 09:15:03 +0300

    Added error handling mechanism

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 		c.HTML(http.StatusOK, "css.tmpl", nil)
 	})
 
-	stu1 := &student{Name: "ginktutu", Age: 20}
+	stu1 := &student{Name: "Daniel", Age: 20}
 	stu2 := &student{Name: "Jack", Age: 22}
 
 	r.GET("/students", func(c *gin.Context) {
@@ -45,9 +45,19 @@ func main() {
 	r.GET("/date", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "custom_func.tmpl", gin.H{
 			"title": "gin",
-			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
+			"now":   time.Date(2020, 9, 18, 0, 0, 0, 0, time.UTC),
 		})
 	})
 
-	r.Run(":9999")
+	r2 := gin.Default()
+	r2.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World\n")
+	})
+	// index out of range for testing Recovery()
+	r2.GET("/panic", func(c *gin.Context) {
+		names := []string{"Daniel"}
+		c.String(http.StatusOK, names[100])
+	})
+
+	r2.Run(":9998")
 }
